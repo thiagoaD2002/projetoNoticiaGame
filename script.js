22b7563a75306fb93153afc94d4ae4c97efb6faa
Thiago <100088838+onlythiagoad@users.noreply.github.com>
2024-12-16 17:55:48 -0300

    novas att

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ liNav.forEach((el,index,array) => {
 
     const telaInicial = document.querySelector(".telaInicial")
     const telaRecente = document.querySelector(".telaRecentes")
-
+    const telaDestaque = document.querySelector(".telaDestaque")
    
 
     array[0].addEventListener("click",()=>{
         telaInicial.style.display = 'block'
         telaRecente.style.display = 'none'
+        telaDestaque.style.display = 'none'
     })
 
     array[1].addEventListener("click",()=>{
+        telaDestaque.style.display = 'none'
         telaInicial.style.display = 'none'
         telaRecente.style.display = 'block'
+    })
+
+    array[2].addEventListener("click",() =>{
+        telaInicial.style.display = 'none'
+        telaRecente.style.display = 'none'
+        telaDestaque.style.display = 'block'
     })
 })
